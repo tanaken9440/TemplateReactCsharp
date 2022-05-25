591442a1206b270d2e30361d70dd7cdbada78e66
田中 健太郎 <tanaka@e-macro.ne.jp>
2022-05-25 17:37:06 +0900

    EFCoreのリバースエンジニアリングについて注記

--- TemplateReactCsharp.DB/DBContext.cs
+++ TemplateReactCsharp.DB/DBContext.cs
@@ -6,6 +6,11 @@ using System.IO;
 
 namespace TemplateReactCsharp.DB
 {
+    /// <summary>
+    /// 以下のコマンドを使用して、既存データベースからモデルを自動生成する
+    /// dotnet ef dbcontext scaffold "<接続文字列>" <プロバイダー名> --output-dir Models --context-dir . --context DBContext --namespace TemplateReactCsharp.DB.Models
+    /// https://docs.microsoft.com/ja-jp/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
+    /// </summary>
     public class DBContext : DbContext
     {
         public DBContext(DbContextOptions<DBContext> options) : base(options) { }
